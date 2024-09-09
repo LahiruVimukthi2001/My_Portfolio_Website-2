81c12c1ec61c3095564b7aed1460a217be9ce112
Lahiru Vimukthi <93706077+LahiruVimukthi2001@users.noreply.github.com>
2024-09-09 20:42:26 +0530

    add testimonials variables js

--- assets/JavaScript/Script.js
+++ assets/JavaScript/Script.js
@@ -9,3 +9,9 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
