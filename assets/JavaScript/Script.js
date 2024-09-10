05e44178a12382ed402ca08efbe7004b8fb20b15
Lahiru Vimukthi <93706077+LahiruVimukthi2001@users.noreply.github.com>
2024-09-10 20:31:02 +0530

    add modal variable js

--- assets/JavaScript/Script.js
+++ assets/JavaScript/Script.js
@@ -15,3 +15,8 @@ const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
